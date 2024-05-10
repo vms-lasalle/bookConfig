d448a8ab4a538145713298896312e5d72ce7ceec
Víctor Manuel Sánchez <vmsanchez@lasallegranvia.es>
2024-05-10 15:50:10 +0200

    versión alternativa de userStore

--- src/__tests__/user.spec.js
+++ src/__tests__/user.spec.js
@@ -1,23 +1,16 @@
 import { setActivePinia, createPinia } from 'pinia'
 import { useUserStore } from '@/stores/user'
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import axios from 'axios'
+
+vi.mock('axios')
 
 describe('User Store', () => {
     beforeEach(() => {
         setActivePinia(createPinia())
     })
 
-    it('should have a default API host', () => {
-        const userStore = useUserStore()
-        expect(userStore.apiHost).toEqual('http://slimphp.local')
-    })
-
-    it('should have a default API port', () => {
-        const userStore = useUserStore()
-        expect(userStore.apiPort).toEqual('8888')
-    })
-
-    it('should have a computed API URL', () => {
+    it('should have an API URL', () => {
         const userStore = useUserStore()
         expect(userStore.apiUrl).toEqual('http://slimphp.local:8888/')
     })
@@ -25,6 +18,7 @@ describe('User Store', () => {
     /*     it('should login correctly', async () => {
         const userStore = useUserStore()
         userStore.login('vmsanchez@lasallegranvia.es', 'pasar')
-        expect(userStore.token).toBeNull()
+        console.log(userStore.loginData)
+        expect(userStore.loginData.userMail).toBe('vmsanchez@lasallegranvia.es')
     }) */
 })
--- src/stores/user.js
+++ src/stores/user.js
@@ -6,35 +6,42 @@ export const useUserStore = defineStore('user', () => {
     const userMail = ref(null)
     const token = ref(null)
     const error = ref(null)
-    const apiHost = 'http://slimphp.local'
-    const apiPort = '8888'
+    const apiUrl = 'http://slimphp.local:8888/'
 
-    const apiUrl = computed({
+    const loginData = computed({
         get() {
-            return apiHost + ':' + apiPort + '/'
+            return {
+                userMail: userMail.value,
+                token: token.value,
+                error: error.value
+            }
         }
     })
 
     async function login(email, password) {
         try {
-            let response = await axios.post(apiUrl.value + 'api/auth/login', {
+            const res = await axios.post(apiUrl + 'api/auth/login', {
                 identifier: email,
                 password: password
             })
-            console.log('login correct')
-            userMail.value = response.data.user
-            token.value = response.data.jwt
+            if (res.data.success) {
+                userMail.value = res.data.user.email
+                token.value = res.data.jwt
+                error.value = null
+            } else {
+                userMail.value = null
+                token.value = null
+                error.value = res.data.message
+            }
         } catch (e) {
-            error.value = 'An error ocurred: ' + e.response
+            userMail.value = null
+            token.value = null
+            error.value = 'An error ocurred'
         }
     }
 
     return {
-        userMail,
-        token,
-        error,
-        apiHost,
-        apiPort,
+        loginData,
         apiUrl,
         login
     }
