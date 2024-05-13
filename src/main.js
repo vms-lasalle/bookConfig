13b8f85dea8ba72062eb53fb35d55a95a514ae88
Víctor Manuel Sánchez <vmsanchez@lasallegranvia.es>
2024-05-13 20:30:15 +0200

    Añadir route guards globales

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from './router/index'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import { useAuthStore } from '@/stores/AuthStore'
 
 const app = createApp(App)
 
-app.use(createPinia())
+router.beforeEach((to) => {
+    const authStore = useAuthStore()
+
+    const isAuthenticated = authStore.user.token ? true : false
+
+    if (isAuthenticated && to.name === 'login') {
+        return { name: 'home' }
+    }
+
+    if (!isAuthenticated && to.name !== 'login') {
+        return { name: 'login' }
+    }
+})
+
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
