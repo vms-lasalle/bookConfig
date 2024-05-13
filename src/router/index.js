d1068b6b721385efaec91588d3ebd3845716f64d
Víctor Manuel Sánchez <vmsanchez@lasallegranvia.es>
2024-05-13 08:40:55 +0200

    Ordenar codigo del router

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/users',
+        name: 'users',
+        component: () => import('@/views/UsersView.vue')
+    },
+    {
+        path: '/stages',
+        name: 'stages',
+        component: () => import('@/views/StagesView.vue')
+    },
+    {
+        path: '/subjects',
+        name: 'subjects',
+        component: () => import('@/views/SubjectsView.vue')
+    },
+    {
+        path: '/languages',
+        name: 'languages',
+        component: () => import('@/views/LanguagesView.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/LoginView.vue')
+    }
+]
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView
-        },
-        {
-            path: '/users',
-            name: 'users',
-            component: () => import('@/views/UsersView.vue')
-        },
-        {
-            path: '/stages',
-            name: 'stages',
-            component: () => import('@/views/StagesView.vue')
-        },
-        {
-            path: '/subjects',
-            name: 'subjects',
-            component: () => import('@/views/SubjectsView.vue')
-        },
-        {
-            path: '/languages',
-            name: 'languages',
-            component: () => import('@/views/LanguagesView.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('@/views/LoginView.vue')
-        }
-    ]
+    routes
 })
 
 export default router
