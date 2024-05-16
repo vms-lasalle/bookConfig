91dabbd677c11672953368700d54fbcd14a5a35f
Víctor Manuel Sánchez <vmsanchez@lasallegranvia.es>
2024-05-16 15:45:44 +0200

    Completar implementación de StagesView

--- src/__tests__/StagesView.spec.js
+++ src/__tests__/StagesView.spec.js
@@ -1,11 +1,16 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import StagesView from '@/views/StagesView.vue'
+import { setActivePinia, createPinia } from 'pinia'
 
 describe('StagesView', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+
     it('StagesView renders properly', async () => {
         const wrapper = shallowMount(StagesView)
         // h1 contains Stages
-        expect(wrapper.find('h1').text()).toContain('Stages')
+        expect(wrapper.find('h1').text()).toContain('Niveles educativos')
     })
 })
