f001a71c6f9f780f3d84ceb53275b5d77292d228
Víctor Manuel Sánchez <vmsanchez@lasallegranvia.es>
2024-05-28 10:09:26 +0200

    añadir url de producción en comentario

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = defineStore('authStore', () => {
     const user = ref({})
     const error = ref(null)
     const apiUrl = 'http://slimphp.local:8888'
+    // const apiUrl = 'http://utilesnca.vetorius.ovh:1337'
 
     const login = (credentials) => {
         axios
